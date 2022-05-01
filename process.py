6e5aa95f5c9b8257ffd8033f808401f239796987
Sphinx <twsphinx54@gmail.com>
2022-05-01 23:10:56 +0800

    basic

---
+++ process.py
@@ -0,0 +1,40 @@
+import os
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = './img'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+app = Flask(__name__, template_folder="./webpage", static_folder='./webpage', static_url_path="")
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'GET':
+        return render_template("index.html")
+    elif request.method == 'POST':
+        print('yes')
+        print(request.values)
+
+        # # check if the post request has the file part
+        # if 'file1' not in request.files:
+        #     flash('No file part')
+        #     return redirect(request.url)
+        file = request.files['file1']
+        # If the user does not select a file, the browser submits an empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return render_template("main.html")
+
+
+if __name__ == '__main__':
+    app.run(port=8080)
