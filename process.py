87435abf44dd4c94fd89447bdcce4c79720366b5
Sphinx <twsphinx54@gmail.com>
2022-05-02 00:14:33 +0800

    save_pic

--- process.py
+++ process.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, flash, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = './img'
+UPLOAD_FOLDER = './imgs'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__, template_folder="./webpage", static_folder='./webpage', static_url_path="")
@@ -16,25 +16,25 @@ def allowed_file(filename):
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'GET':
-        return render_template("index.html")
+        return render_template('index.html')
     elif request.method == 'POST':
-        print('yes')
-        print(request.values)
-        print(request.__class__)
-
-        # # check if the post request has the file part
-        # if 'file1' not in request.files:
-        #     flash('No file part')
-        #     return redirect(request.url)
-        file = request.files['file1']
+        file1 = request.files['image1']
+        file2 = request.files['image2']
         # If the user does not select a file, the browser submits an empty file without a filename.
-        if file.filename == '':
+        if (file1.filename == '') | (file2.filename == ''):
             flash('No selected file')
             return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return render_template("main.html")
+        if file1 and allowed_file(file1.filename) and file2 and allowed_file(file2.filename):
+            filename1 = secure_filename(file1.filename)
+            file1.save(os.path.join(app.config['UPLOAD_FOLDER'], filename1))
+            filename2 = secure_filename(file2.filename)
+            file2.save(os.path.join(app.config['UPLOAD_FOLDER'], filename2))
+            return main_process()
+
+
+@app.route('/result', methods=['GET', 'POST'])
+def main_process():
+    return render_template('main.html')
 
 
 if __name__ == '__main__':
